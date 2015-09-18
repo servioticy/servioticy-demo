575150c77a15d46a0a493036fa0f0af13170298b
davidcarrera <dcarrera@ac.upc.edu>
2015-09-18 13:02:33 +0200

    rate-based load

---
+++ high_rate_data.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+. env.sh
+
+START_FOLDER=$PWD
+
+mkdir -p $IDS_FOLDER
+
+echo
+echo "*********************************"
+echo servIoTicy final demo tester
+echo  Generating a sensor update
+echo "*********************************"
+echo
+
+$SCRIPTS/get_access_token.sh $START_FOLDER
+$SCRIPTS/get_random_access_token.sh $START_FOLDER
+$SCRIPTS/push_data_so_at_rate.sh $START_FOLDER 10
---
+++ scripts/push_data_so_at_rate.sh
@@ -0,0 +1,35 @@
+rate=$2
+sleep_time=$( echo "scale=4; 1/$rate" | bc)
+
+
+echo time: $time, rate: $rate, sleep: $sleep_time
+
+
+while :
+do
+
+for file in `ls $SOS_FOLDER`
+do
+ id=`cat $1/$IDS_FOLDER/$file.id | perl -pe "s/\"/\n/g" | head -4 | tail -1`
+ at=`cat $1/$IDS_FOLDER/$file.id | perl -pe "s/\"/\n/g" | head -8 | tail -1`
+
+ response=$(curl --digest -XPUT \
+     -H "Content-Type: application/json;charset=UTF-8" \
+     -H "Authorization: $at"  \
+     --write-out %{http_code} -s\
+     -d @$1/$SODATA_FOLDER/$file \
+     -o /dev/null \
+     http://$API_PUB_NODES:$API_PUB_SEC_PORT/$id/streams/$SAMPLE_STREAM)
+
+
+     if [ $response != 202 ];
+     then
+                echo "KO... Error pushing data to SO based on $file -> response: "$response
+     else
+                echo "OK... Pushed data to SO based on $file, ID: "$id
+     fi
+done
+
+sleep $sleep_time
+
+done
